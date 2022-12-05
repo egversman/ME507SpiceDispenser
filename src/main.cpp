b5d5580bdf1832e50ec74e980d2a003603367c16
egversman <107862201+egversman@users.noreply.github.com>
2022-12-04 17:59:36 -0800

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@
 #include <shares.h>
 
 
-
 Share<float> current_position ("Position");
 Queue<float> current_weight (100, "Weight");
 Share<bool> initalized("initalized");
@@ -35,8 +34,8 @@ void setup()
     //xTaskCreate (task_encoder, "Encoder", 5120, NULL, 6, NULL);
     //xTaskCreate (task_carousel, "Carousel Motor", 5120, NULL, 4, NULL);
     //xTaskCreate (task_door, "Door Motor", 5120, NULL, 3, NULL);
-   // xTaskCreate (task_load_cell, "Load Cell", 5120, NULL, 5, NULL);
-   // xTaskCreate (task_opening, "Carousel Motor", 5120, NULL, 2, NULL);
+    // xTaskCreate (task_load_cell, "Load Cell", 5120, NULL, 5, NULL);
+    // xTaskCreate (task_opening, "Carousel Motor", 5120, NULL, 2, NULL);
     // xTaskCreate (FSM, "Finite State Machine", 5120, NULL, 1, NULL);
 
 }
